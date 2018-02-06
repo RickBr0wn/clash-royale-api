86ca297ffb8bfc07bc56f72318fee84cc2310932
RiCK BROWN <ricky.brown.00@gmail.com>
2018-02-06 02:43:30 +0000

    Initial commit

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    console.log(api_key);
+      $('#buttonSubmit').click(function(){
+        // let playerTag = $('#tagInput').val();
+        let playerTag = "RYULJJJJ";
+        let settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "http://api.cr-api.com/player/" + playerTag,
+            "method": "GET",
+            "headers": {
+              "auth": api_key
+            }
+          }
+
+        if(playerTag === ""){
+          alert('Please enter a clantag!!');
+        }     
+
+        $.ajax(settings).done(function (response) {
+          console.log(response);
+          document.getElementById('nameHolder').innerHTML = "Name: " + response.name;
+          document.getElementById('trophiesHolder').innerHTML = "Trophies: " + response.trophies;
+          document.getElementById('arenaHolder').innerHTML = "Arena: " + response.arena.arena + " - " + response.arena.name;
+
+          $('#currentDeck').css("background-color: ", "yellow");
+          // "url(" + response.currentDeck[0].icon + ")");
+
+          document.getElementById('cycleTitle').innerHTML = "The following numbers are the number of <strong>free</strong> chests that need to be won before you are awarded the appropiate chest.";
+          document.getElementById('epicCycle').innerHTML = "Epic Chest: " + response.chestCycle.epic;
+          document.getElementById('giantCycle').innerHTML = "Giant Chest: " + response.chestCycle.giant;
+          document.getElementById('magicalCycle').innerHTML = "Magical Chest: " + response.chestCycle.magical;
+          document.getElementById('superMagicalCycle').innerHTML = "Super Magical Chest: " + response.chestCycle.superMagical;
+          document.getElementById('legendaryCycle').innerHTML = "Legendary Chest: " + response.chestCycle.legendary;
+        });
+      });
+});
